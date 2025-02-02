d5363133c38eeb7fe087b29597207f9b9da15d16
Derek Smith <derek.smith@nvoq.com>
2025-02-02 02:41:55 +0000

    Implement adders and ALU

--- gates.py
+++ gates.py
@@ -1,14 +1,16 @@
+##########################################################
+# This is our axiomatic gate, so we can create it
+# with high-level Python constructs.
+##########################################################
+
 def Nand(a, b):
-    '''
-    This is our axiomatic gate, so we can create it with
-    high-level Python constructs.
-    '''
     return 0 if a == 1 and b == 1 else 1
 
 
-####################################
-# Now we implement the basic gates #
-####################################
+##########################################################
+# Now we implement the basic gates using only Nand
+# and previously-defined gates
+##########################################################
 
 def Not(a):
     return Nand(a, a)
@@ -34,9 +36,9 @@ def Demux(inp, sel):
     return a, b
 
 
-#################################################
-# Multi-bit versions of some of the basic gates #
-#################################################
+##########################################################
+# Multi-bit versions of some of the basic gates
+##########################################################
 
 def Not16(inp):
     out0  = Not(inp[15])
@@ -59,44 +61,44 @@ def Not16(inp):
     return out15, out14, out13, out12, out11, out10, out9, out8, \
            out7,  out6,  out5,  out4,  out3,  out2,  out1, out0
 
-def And16(inp):
-    out0  = And(inp[15])
-    out1  = And(inp[14])
-    out2  = And(inp[13])
-    out3  = And(inp[12])
-    out4  = And(inp[11])
-    out5  = And(inp[10])
-    out6  = And(inp[9])
-    out7  = And(inp[8])
-    out8  = And(inp[7])
-    out9  = And(inp[6])
-    out10 = And(inp[5])
-    out11 = And(inp[4])
-    out12 = And(inp[3])
-    out13 = And(inp[2])
-    out14 = And(inp[1])
-    out15 = And(inp[0])
+def And16(a, b):
+    out0  = And(a[15], b[15])
+    out1  = And(a[14], b[14])
+    out2  = And(a[13], b[13])
+    out3  = And(a[12], b[12])
+    out4  = And(a[11], b[11])
+    out5  = And(a[10], b[10])
+    out6  = And(a[9],  b[9])
+    out7  = And(a[8],  b[8])
+    out8  = And(a[7],  b[7])
+    out9  = And(a[6],  b[6])
+    out10 = And(a[5],  b[5])
+    out11 = And(a[4],  b[4])
+    out12 = And(a[3],  b[3])
+    out13 = And(a[2],  b[2])
+    out14 = And(a[1],  b[1])
+    out15 = And(a[0],  b[0])
 
     return out15, out14, out13, out12, out11, out10, out9, out8, \
            out7,  out6,  out5,  out4,  out3,  out2,  out1, out0
 
-def Or16(inp):
-    out0  = Or(inp[15])
-    out1  = Or(inp[14])
-    out2  = Or(inp[13])
-    out3  = Or(inp[12])
-    out4  = Or(inp[11])
-    out5  = Or(inp[10])
-    out6  = Or(inp[9])
-    out7  = Or(inp[8])
-    out8  = Or(inp[7])
-    out9  = Or(inp[6])
-    out10 = Or(inp[5])
-    out11 = Or(inp[4])
-    out12 = Or(inp[3])
-    out13 = Or(inp[2])
-    out14 = Or(inp[1])
-    out15 = Or(inp[0])
+def Or16(a, b):
+    out0  = Or(a[15], b[15])
+    out1  = Or(a[14], b[14])
+    out2  = Or(a[13], b[13])
+    out3  = Or(a[12], b[12])
+    out4  = Or(a[11], b[11])
+    out5  = Or(a[10], b[10])
+    out6  = Or(a[9],  b[9])
+    out7  = Or(a[8],  b[8])
+    out8  = Or(a[7],  b[7])
+    out9  = Or(a[6],  b[6])
+    out10 = Or(a[5],  b[5])
+    out11 = Or(a[4],  b[4])
+    out12 = Or(a[3],  b[3])
+    out13 = Or(a[2],  b[2])
+    out14 = Or(a[1],  b[1])
+    out15 = Or(a[0],  b[0])
 
     return out15, out14, out13, out12, out11, out10, out9, out8, \
            out7,  out6,  out5,  out4,  out3,  out2,  out1, out0
@@ -123,9 +125,9 @@ def Mux16(a, b, sel):
            out7,  out6,  out5,  out4,  out3,  out2,  out1, out0
 
 
-#################################################
-# Multi-way versions of some of the basic gates #
-#################################################
+##########################################################
+# Multi-way versions of some of the basic gates
+##########################################################
 
 def Or8Way(inp):
     return Or(inp[7],
@@ -194,3 +196,67 @@ def Demux8Way(inp, sel):
 
     return a, b, c, d, e, f, g, h
     
+
+
+def HalfAdder(a, b):
+    sm = Xor(a, b)
+    cr = And(a, b)
+    return sm, cr
+
+def FullAdder(a, b, c):
+    s0, c0 = HalfAdder(a, b)
+    sm, c1 = HalfAdder(s0, c)
+    cr = Or(c0, c1)
+    return sm, cr
+
+def Add16(a, b):
+    sm0,  cr0  = HalfAdder(a[15], b[15])
+    sm1,  cr1  = FullAdder(a[14], b[14], cr0)
+    sm2,  cr2  = FullAdder(a[13], b[13], cr1)
+    sm3,  cr3  = FullAdder(a[12], b[12], cr2)
+    sm4,  cr4  = FullAdder(a[11], b[11], cr3)
+    sm5,  cr5  = FullAdder(a[10], b[10], cr4)
+    sm6,  cr6  = FullAdder(a[9],  b[9],  cr5)
+    sm7,  cr7  = FullAdder(a[8],  b[8],  cr6)
+    sm8,  cr8  = FullAdder(a[7],  b[7],  cr7)
+    sm9,  cr9  = FullAdder(a[6],  b[6],  cr8)
+    sm10, cr10 = FullAdder(a[5],  b[5],  cr9)
+    sm11, cr11 = FullAdder(a[4],  b[4],  cr10)
+    sm12, cr12 = FullAdder(a[3],  b[3],  cr11)
+    sm13, cr13 = FullAdder(a[2],  b[2],  cr12)
+    sm14, cr14 = FullAdder(a[1],  b[1],  cr13)
+    sm15, _    = FullAdder(a[0],  b[0],  cr14)
+
+    return sm15, sm14, sm13, sm12, sm11, sm10, sm9, sm8, \
+           sm7,  sm6,  sm5,  sm4,  sm3,  sm2,  sm1, sm0
+
+def Inc16(inp):
+    # TODO: This is what you're not supposed to do
+    return Add16(inp,
+                 (0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1))
+
+def ALU(x, y, zx, nx, zy, ny, f, no):
+    zero16 = (0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
+
+    mid_x = Mux16(x, zero16, zx)
+
+    end_x = Mux16(mid_x, Not16(mid_x), nx)
+
+    mid_y = Mux16(y, zero16, zy)
+
+    end_y = Mux16(mid_y, Not16(mid_y), ny)
+
+    mid_out = Mux16(And16(end_x, end_y),
+                    Add16(end_x, end_y),
+                    f)
+
+    out = Mux16(mid_out, Not16(mid_out), no)
+
+    zr = Not(Or(Or8Way((out[15], out[14], out[13], out[12],
+                        out[11], out[10], out[9], out[8])),
+                Or8Way((out[7],  out[6],  out[5],  out[4],
+                        out[3],  out[2],  out[1], out[0]))))
+    
+    ng = out[0]
+
+    return out, zr, ng
