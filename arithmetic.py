553a1aef89934b0ec091196fe80bcdcf9f45f85d
Derek Smith <derek.smith@nvoq.com>
2025-02-17 18:24:32 +0000

    Implement assembler; reogranize things into modules

---
+++ arithmetic.py
@@ -0,0 +1,64 @@
+from gates import *
+
+
+def HalfAdder(a, b):
+    sm = Xor(a, b)
+    cr = And(a, b)
+    return sm, cr
+
+def FullAdder(a, b, c):
+    s0, c0 = HalfAdder(a, b)
+    sm, c1 = HalfAdder(s0, c)
+    cr = Or(c0, c1)
+    return sm, cr
+
+def Add16(a, b):
+    sm0,  cr0  = HalfAdder(a[15], b[15])
+    sm1,  cr1  = FullAdder(a[14], b[14], cr0)
+    sm2,  cr2  = FullAdder(a[13], b[13], cr1)
+    sm3,  cr3  = FullAdder(a[12], b[12], cr2)
+    sm4,  cr4  = FullAdder(a[11], b[11], cr3)
+    sm5,  cr5  = FullAdder(a[10], b[10], cr4)
+    sm6,  cr6  = FullAdder(a[9],  b[9],  cr5)
+    sm7,  cr7  = FullAdder(a[8],  b[8],  cr6)
+    sm8,  cr8  = FullAdder(a[7],  b[7],  cr7)
+    sm9,  cr9  = FullAdder(a[6],  b[6],  cr8)
+    sm10, cr10 = FullAdder(a[5],  b[5],  cr9)
+    sm11, cr11 = FullAdder(a[4],  b[4],  cr10)
+    sm12, cr12 = FullAdder(a[3],  b[3],  cr11)
+    sm13, cr13 = FullAdder(a[2],  b[2],  cr12)
+    sm14, cr14 = FullAdder(a[1],  b[1],  cr13)
+    sm15, _    = FullAdder(a[0],  b[0],  cr14)
+
+    return sm15, sm14, sm13, sm12, sm11, sm10, sm9, sm8, \
+           sm7,  sm6,  sm5,  sm4,  sm3,  sm2,  sm1, sm0
+
+def Inc16(inp):
+    return Add16(inp,
+                 (0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1))
+
+def ALU(x, y, zx, nx, zy, ny, f, no):
+    zero16 = (0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
+
+    mid_x = Mux16(x, zero16, zx)
+
+    end_x = Mux16(mid_x, Not16(mid_x), nx)
+
+    mid_y = Mux16(y, zero16, zy)
+
+    end_y = Mux16(mid_y, Not16(mid_y), ny)
+
+    mid_out = Mux16(And16(end_x, end_y),
+                    Add16(end_x, end_y),
+                    f)
+
+    out = Mux16(mid_out, Not16(mid_out), no)
+
+    zr = Not(Or(Or8Way((out[15], out[14], out[13], out[12],
+                        out[11], out[10], out[9], out[8])),
+                Or8Way((out[7],  out[6],  out[5], out[4],
+                        out[3],  out[2],  out[1], out[0]))))
+    
+    ng = out[0]
+
+    return out, zr, ng
---
+++ assembler.py
@@ -0,0 +1,192 @@
+from enum import Enum
+from pathlib import Path
+
+
+class InstructionType(Enum):
+    A_INSTRUCTION = 0
+    C_INSTRUCTION = 1
+    L_INSTRUCTION = 2
+
+
+class Parser:
+    def __init__(self, asm_path):
+        with open(asm_path, 'r') as f:
+            self.lines = f.readlines()
+
+        self.current_instruction = None
+    
+    @property
+    def has_more_lines(self):
+        return len(self.lines) > 0
+    
+    def advance(self):
+        while True:
+            if len(self.lines) == 0:
+                return
+            
+            line = self.lines.pop(0).strip()
+            if line != '' and not line.startswith('//'):
+                # Remove any trailing comments
+                self.current_instruction = line.split()[0]
+                break
+    
+    @property
+    def instruction_type(self):
+        if self.current_instruction.startswith('@'):
+            return InstructionType.A_INSTRUCTION
+        elif self.current_instruction.startswith('('):
+            return InstructionType.L_INSTRUCTION
+        else:
+            return InstructionType.C_INSTRUCTION
+
+    @property
+    def symbol(self):
+        if self.instruction_type == InstructionType.L_INSTRUCTION:
+            # L-instruction: return the text between "(" at the
+            # beginning and the first ")"
+            return self.current_instruction.split(')')[0][1:]
+        else:
+            # A-instruction: Return text after "@" at the beginning
+            return self.current_instruction[1:]
+
+    # For the following three methods: C-instructions are formatted
+    # dest=comp;jump
+
+    @property
+    def dest(self):
+        if '=' not in self.current_instruction:
+            return None
+        
+        return self.current_instruction.split('=')[0]
+
+    @property
+    def comp(self):
+        instruction = self.current_instruction
+        
+        if '=' in instruction:
+            instruction = instruction.split('=')[1]
+        
+        if ';' in instruction:
+            instruction = instruction.split(';')[0]
+        
+        return instruction
+    
+    @property
+    def jump(self):
+        if ';' not in self.current_instruction:
+            return None
+        
+        return self.current_instruction.split(';')[1]
+
+
+class Code:
+    def dest(s):
+        if s is None:
+            return '000'
+
+        a_bit = '1' if 'A' in s else '0'
+        d_bit = '1' if 'D' in s else '0'
+        m_bit = '1' if 'M' in s else '0'
+
+        return a_bit + d_bit + m_bit
+    
+    def comp(s):
+        r, a = ('M', '1') if 'M' in s else ('A', '0')
+        comp2binary = {
+            '0':    '101010',
+            '1':    '111111',
+            '-1':   '111010',
+            'D':    '001100',
+            r:      '110000',
+            '!D':   '001101',
+            '!'+r:  '110001',
+            '-D':   '001111',
+            '-'+r:  '110011',
+            'D+1':  '011111',
+            r+'+1': '110111',
+            'D-1':  '001110',
+            r+'-1': '110010',
+            'D+'+r: '000010',
+            'D-'+r: '010011',
+            r+'-D': '000111',
+            'D&'+r: '000000',
+            'D|'+r: '010101', 
+        }
+
+        cccccc = comp2binary[s]
+        return a + cccccc
+
+    def jump(s):
+        return {
+            None:  '000',
+            'JGT': '001',
+            'JEQ': '010',
+            'JGE': '011',
+            'JLT': '100',
+            'JNE': '101',
+            'JLE': '110',
+            'JMP': '111'
+        }[s]
+
+
+class SymbolTable(dict):
+    def __init__(self):
+        for i in range(16):
+            self[f'R{i}'] = i
+        
+        self['SP'] = 0
+        self['LCL'] = 1
+        self['ARG'] = 2
+        self['THIS'] = 3
+        self['THAT'] = 4
+
+        self['SCREEN'] = 16384
+        self['KBD'] = 24576
+
+
+class HackAssembler:
+    def __init__(self):
+        self.symbol_table = SymbolTable()
+    
+    def translate(self, asm_path):
+        self._first_pass(asm_path)
+        self._second_pass(asm_path)
+    
+    def _first_pass(self, asm_path):
+        line_number = 0
+        parser = Parser(asm_path)
+        while parser.has_more_lines:
+            parser.advance()
+
+            if parser.instruction_type == InstructionType.L_INSTRUCTION:
+                self.symbol_table[parser.symbol] = line_number
+            else:
+                line_number += 1
+    
+    def _second_pass(self, asm_path):
+        next_available_address = 16
+        hack_path = Path(asm_path).with_suffix('.hack')
+        with open(hack_path, 'w') as f:
+            parser = Parser(asm_path)
+            while parser.has_more_lines:
+                parser.advance()
+
+                if parser.instruction_type == InstructionType.C_INSTRUCTION:
+                    acccccc = Code.comp(parser.comp)
+                    ddd = Code.dest(parser.dest)
+                    jjj = Code.jump(parser.jump)
+                    f.write(f'111{acccccc}{ddd}{jjj}\n')
+                elif parser.instruction_type == InstructionType.A_INSTRUCTION:
+                    if not parser.symbol.isdigit():
+                        if not parser.symbol in self.symbol_table:
+                            self.symbol_table[parser.symbol] = next_available_address
+                            next_available_address += 1
+                        
+                        address = str(self.symbol_table[parser.symbol])
+                    else:
+                        address = parser.symbol
+                    
+                    f.write('0' + f'{int(address):015b}\n')
+                else: # L-instructions were processed in the first pass
+                    continue
+                
