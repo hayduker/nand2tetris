59f7163cd9e6199a8b9c6e2785fcf663e15a9b4e
Derek Smith <github@sandy.simplelogin.com>
2025-07-20 15:16:33 -0600

    First half of VM is working!

--- vm.py
+++ vm.py
@@ -1,7 +1,7 @@
 from enum import Enum
 from pathlib import Path
 from dataclasses import dataclass
-
+from collections.abc import Callable
 
 class CommandType(Enum):
     ARITHMETIC = 1
@@ -39,15 +39,18 @@ class Parser:
         return self
     
     def __next__(self):
-        line = next(self.file)
-        splat = line.strip().split()
+        line = ''
+        while line == '' or line.startswith('//'):
+            line = next(self.file).strip()            
+
+        splat = line.split()
         command = splat[0]
 
         if command in ['push', 'pop']:
             return VMCommand(
                 type=CommandType.PUSH if command == 'push' else CommandType.POP,
                 arg1=splat[1],
-                arg2=splat[2]
+                arg2=int(splat[2])
             )
         
         elif command in ['add', 'sub', 'neg', 'eq', 'gt', 'lt', 'and', 'or', 'not']:
@@ -62,12 +65,16 @@ segment2symbol = {
     'argument': 'ARG',
     'this': 'THIS',
     'that': 'THAT',
-    'temp': 'TEMP',
 }
 
 class CodeWriter:
+    POINTER_OFFSET = 3
+    TEMP_OFFSET = 5
+    
     def __init__(self, output_file: Path):
         self.output_file = output_file
+        self.static_name = output_file.stem
+        self.next_label = self._create_label_maker()
 
     def __enter__(self):
         self.file = open(self.output_file, 'w')
@@ -75,86 +82,207 @@ class CodeWriter:
 
     def __exit__(self, exc_type, exc_value, exc_tb):
         if self.file:
+            self._write_end_loop()
             self.file.close()
 
     def write_arithmetic(self, command: str) -> None:
         if command == 'add':
-            ## Pop first to R13
-            # SP--
-            self._write(f'@SP')
-            self._write(f'M=M-1')
-            # D = RAM[SP]
-            self._write(f'@SP')
-            self._write(f'A=M')
-            self._write(f'D=M')
-            # R13 = D
-            self._write(f'@R13')
-            self._write(f'M=D')
+            self._write_binary_operation('+')
 
-            ## Pop second to D
-            # SP--
-            self._write(f'@SP')
-            self._write(f'M=M-1')
-            # D = RAM[SP]
-            self._write(f'@SP')
-            self._write(f'A=M')
-            self._write(f'D=M')
-            
-            ## D += R13
-            self._write(f'@R13')
-            self._write(f'D=D+M')
+        if command == 'sub':
+            self._write_binary_operation('-')
+        
+        if command == 'neg':
+            self._write_unary_operation('-')
+        
+        if command == 'eq':
+            self._write_compare_operation('==')
 
-            ## Push D to stack
-            # RAM[SP] = D
-            self._write(f'@SP')
-            self._write(f'A=M')
-            self._write(f'M=D')
-            # SP++
-            self._write(f'@SP')
-            self._write(f'M=M+1')
+        if command == 'gt':
+            self._write_compare_operation('>')
+        
+        if command == 'lt':
+            self._write_compare_operation('<')
 
+        if command == 'and':
+            self._write_binary_operation('&')
 
+        if command == 'or':
+            self._write_binary_operation('|')
+        
+        if command == 'not':
+            self._write_unary_operation('!')
 
     def write_push_pop(self, command, segment: str, index: int) -> None:
-        symbol = segment2symbol[segment]
-
         if command == CommandType.PUSH:
-            # D = segment[index]
-            self._write(f'@{symbol}')
-            self._write(f'D=A')
-            self._write(f'@{index}')
-            self._write(f'A=D+A')
-            self._write(f'D=M')
-            # RAM[SP] = D
-            self._write(f'@SP')
-            self._write(f'A=M')
-            self._write(f'M=D')
-            # SP++
-            self._write(f'@SP')
-            self._write(f'M=M+1')
+            if segment == 'constant':
+                self._push_constant_to_stack(constant=index)
+            else:
+                self._push_memory_to_stack(segment, index)
 
-        elif command == CommandType.POP:
-            # R13 = segment + index
-            self._write(f'@{symbol}')
-            self._write(f'D=A')
-            self._write(f'@{index}')
-            self._write(f'D=D+A')
-            self._write(f'@R13')
-            self._write(f'M=D')
-            # SP--
-            self._write(f'@SP')
-            self._write(f'M=M-1')
-            # D = RAM[SP]
-            # self._write(f'@SP')
-            self._write(f'A=M')
+        elif command == CommandType.POP:               
+            self._pop_stack_to_memory(segment, index)
+
+    def _write_binary_operation(self, op: str) -> None:
+        self._pop_stack_to_variable('R13')
+        self._pop_stack_to_d()
+        
+        self._set_address('R13')
+        self._write(f'D=D{op}M')
+
+        self._push_d_to_stack()
+
+    def _write_unary_operation(self, op: str) -> None:
+        self._pop_stack_to_d()        
+        self._write(f'D={op}D')
+        self._push_d_to_stack()
+
+    def _write_compare_operation(self, op: str) -> None:
+        jump_op = {
+            '==': 'JEQ',
+            '>':  'JGT',
+            '<':  'JLT',
+        }[op]
+
+        self._pop_stack_to_variable('R13')
+        self._pop_stack_to_d()
+
+        ## D -= R13
+        self._set_address('R13')
+        self._write('D=D-M')
+
+        # If operation is true, jump to TRUE label
+        true_label = self.next_label('TRUE')
+        self._set_address(true_label)
+        self._write(f'D;{jump_op}')
+
+        # False block
+        false_label = self.next_label('FALSE')
+        self._write_label(false_label)
+        self._write('D=0')
+        self._push_d_to_stack()
+        after_label = self.next_label('AFTER')
+        self._set_address(after_label)
+        self._write('0;JMP')
+
+        # True block
+        self._write_label(true_label)
+        self._write('D=-1')
+        self._push_d_to_stack()
+
+        self._write_label(after_label)
+
+    def _push_constant_to_stack(self, constant: int) -> None:
+        self._store_value_in_d(constant)
+        self._push_d_to_stack()
+
+    def _push_memory_to_stack(self, segment: str, index: int) -> None:
+        if segment == 'pointer':
+            self._set_address(self.POINTER_OFFSET + index)
+        elif segment == 'temp':
+            self._set_address(self.TEMP_OFFSET + index)
+        elif segment == 'static':
+            self._set_address(f'{self.static_name}.{index}')
+        else:
+            symbol = segment2symbol[segment]
+            self._set_address(symbol)
             self._write(f'D=M')
-            # segment[index] = value @R13
-            self._write(f'@R13')
-            self._write(f'A=M')
-            self._write(f'M=D')
+            self._set_address(index)
+            self._write(f'A=D+A')
+
+        self._write(f'D=M')
+        self._push_d_to_stack()
+
+    def _push_d_to_stack(self) -> None:
+        self._write_d_to_ram_at_sp()
+        self._write_increment_sp()
+
+    def _pop_stack_to_memory(self, segment: str, index: int) -> None:
+        # D = segment + index
+        if segment == 'pointer':
+            self._store_value_in_d(self.POINTER_OFFSET + index)
+        elif segment == 'temp':
+            self._store_value_in_d(self.TEMP_OFFSET + index)
+        elif segment == 'static':
+            self._store_value_in_d(f'{self.static_name}.{index}')
+        else:
+            symbol = segment2symbol[segment]
+            self._set_address(symbol)
+            self._write(f'D=M')
+            self._set_address(index)
+            self._write(f'D=D+A')
+
+        # R13 = D
+        self._set_address('R13')
+        self._write(f'M=D')
+        
+        # D = RAM[SP]
+        self._pop_stack_to_d()
+
+        # RAM[R13] = D
+        self._set_address('R13')
+        self._write(f'A=M')
+        self._write(f'M=D')
+
+    def _pop_stack_to_variable(self, name: str) -> None:
+        self._pop_stack_to_d()
+        self._set_address(name)
+        self._write('M=D')
+
+    def _pop_stack_to_d(self) -> None:
+        self._write_decrement_sp()
+        self._write_ram_at_sp_to_d()
+
+    def _store_value_in_d(self, value: int | str) -> None:
+        self._set_address(value)
+        self._write(f'D=A')
+
+    def _write_ram_at_sp_to_d(self) -> None:
+        self._set_address('SP')
+        self._write(f'A=M')
+        self._write(f'D=M')
+
+    def _write_d_to_ram_at_sp(self) -> None:
+        self._set_address('SP')
+        self._write(f'A=M')
+        self._write(f'M=D')
+
+    def _write_increment_sp(self) -> None:
+        self._set_address('SP')
+        self._write(f'M=M+1')
+
+    def _write_decrement_sp(self) -> None:
+        self._set_address('SP')
+        self._write(f'M=M-1')
+
+    def _write_label(self, label: str) -> None:
+        self._write(f'({label})')
+
+    def _write_end_loop(self) -> None:
+        end_label = self.next_label('END')
+        self._write_label(end_label)
+        self._set_address(end_label)
+        self._write(f'0;JMP')
+
+    def _set_address(self, addr: int | str) -> None:
+        self._write(f'@{addr}')
 
     def _write(self, text: str) -> None:
         self.file.write(text + '\n')
+
+    def _generate_label(self) -> None:
+        return ''.join(random.choices(string.ascii_uppercase, k=10))
+
+    def _create_label_maker(self) -> Callable[[], str]:
+        index = -1
+
+        def next_label(prefix: str = 'LABEL') -> str:
+            nonlocal index
+            index += 1
+            return f'{prefix}{index}'
+        
+        return next_label
+
 
 
 class VMTranslator:
@@ -170,7 +298,7 @@ class VMTranslator:
                     writer.write_push_pop(
                         command=cmd.type,
                         segment=cmd.arg1,
-                        index=cmd.arg    
+                        index=cmd.arg2,
                     )
 
                 elif cmd.type == CommandType.ARITHMETIC:
@@ -180,4 +308,6 @@ class VMTranslator:
 
 
 if __name__ == '__main__':
-    VMTranslator('testfiles/Test.vm')
+    import sys
+    src_file = sys.argv[1]
+    VMTranslator(src_file)
